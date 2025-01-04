8430183da40490a5314d7e61e2f8091fa8b8fa85
Aditya Raj <adityarajmathur2005@gmail.com>
2025-01-04 16:05:48 +0530

    Corrected Path

--- app/Pages/HomePage/page.js
+++ app/Pages/HomePage/page.js
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Link from "next/link";
 import Image from "next/image";
-import booksData from "../../components/bookData";
+import booksData from "@/app/components/bookData";
 import { BackgroundLines } from "../../components/ui/background-lines";
 
 function HomePage() {
--- app/components/bookData.js
+++ app/components/bookData.js
@@ -3,7 +3,7 @@ const booksData = [
     title: "The Vital Question - Why Is Life the Way It Is - Nick Lane",
     description:
       "An exploration of life's origins, addressing fundamental questions about energy and evolution. A groundbreaking perspective on how life works.",
-    link: "books/b1",
+    link: "../books/b1",
     image: "/images/b1.jpg",
     genre: "Science",
   },
@@ -12,7 +12,7 @@ const booksData = [
       "Ikigai: The Japanese Secret to a Long and Happy Life by Héctor García, Francesc Miralles",
     description:
       "Discover the Japanese philosophy of finding purpose and living a meaningful, joyful life. Unlock the secrets to longevity and happiness.",
-    link: "books/b2",
+    link: "../books/b2",
     image: "/images/b2.jpg",
     genre: "Life",
   },
@@ -20,7 +20,7 @@ const booksData = [
     title: "How to Win Friends and Influence People by Dale Carnegie",
     description:
       "A timeless guide to building meaningful relationships and gaining influence. Learn effective communication and leadership skills.",
-    link: "books/b3",
+    link: "../books/b3",
     image: "/images/b3.jpg",
     genre: "Self-Help",
   },
@@ -28,7 +28,7 @@ const booksData = [
     title: "7 Habits of Highly Effective People by Stephen Covey",
     description:
       "Transform your personal and professional life with these seven habits. A proven framework for achieving success and balance.",
-    link: "books/b4",
+    link: "../books/b4",
     image: "/images/b4.jpg",
     genre: "Self-Help",
   },
@@ -37,7 +37,7 @@ const booksData = [
       "Mindset Changing The Way You think To Fulfill Your Potential by Carol S. Dweck",
     description:
       "Understand the power of mindset in shaping your success and happiness. Learn how to cultivate a growth mindset for a fulfilling life.",
-    link: "books/b5",
+    link: "../books/b5",
     image: "/images/b5.jpg",
     genre: "Life",
   },
@@ -46,7 +46,7 @@ const booksData = [
       "Tuesdays with Morrie: An Old Man, a Young Man, and Life's Greatest Lesson by Mitch Albom",
     description:
       "A touching memoir of life's lessons from a dying mentor to his student. Reflect on love, work, and finding meaning in life.",
-    link: "books/b6",
+    link: "../books/b6",
     image: "/images/b6.jpg",
     genre: "Life",
   },
